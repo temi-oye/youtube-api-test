534c91cdccbbec5d584e2818f7ffacafd50cdf90
Temi <temioye30@gmail.com>
2022-07-30 15:53:21 +0100

    completed tutorial code

--- youtube-api-test.py
+++ youtube-api-test.py
@@ -1,13 +1,21 @@
 from dotenv import load_dotenv
 import os
-
+from googleapiclient.discovery import build
 
 def configure():
   load_dotenv()
 
 def main():
   configure()
+  api_key = os.getenv("api_key")
+  youtube = build('youtube', 'v3', developerKey=api_key)
 
-main()
-
-print(os.getenv("api_key"))
+  # there is no consistent method of getting the id or username of a channel as far as I know
+  request = youtube.channels().list(part="statistics", forUsername="GoogleDevelopers")
+  response = request.execute()
+  print(response)
+  
+  youtube.close()
+  
+if __name__ == "__main__":
+  main()
