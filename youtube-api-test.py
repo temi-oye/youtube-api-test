376d6d2024df440461c6292f3ba9008b980223c9
Temi <temioye30@gmail.com>
2022-08-01 20:58:15 +0100

    add func to get duration as iso

--- youtube-api-test.py
+++ youtube-api-test.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from googleapiclient.discovery import build
+import json
 
 def configure():
   load_dotenv()
@@ -15,8 +16,13 @@ def main():
   # response = request.execute()
   # print(response)
 
-  res = get_videos_from_playlist(youtube, "PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS")
-  print(res)
+  # res = get_videos_from_playlist(youtube, "PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS")
+  # print(json.dumps(res, indent=4))
+
+  res = get_duration_as_iso(youtube, "DCDe29sIKcE")
+
+  print(json.dumps(res, indent=4))
+
   
   youtube.close()
 
@@ -24,6 +30,15 @@ def get_videos_from_playlist(youtube, playlist_id):
   request = youtube.playlistItems().list(part="snippet", playlistId=playlist_id)
   response = request.execute()
   return response
+
+def get_duration_as_iso(youtube, video_id):
+  request = youtube.videos().list(part="contentDetails", id=video_id)
+  response = request.execute()
+  iso = response["items"][0]["contentDetails"]["duration"]
+  return iso
+
+  
+
   
 if __name__ == "__main__":
   main()
