984daea8a92c8d5d0bc40e7c29e14df0eba0f25e
Temi <temioye30@gmail.com>
2022-08-01 18:50:44 +0100

    added ability to get videos from playlist

--- youtube-api-test.py
+++ youtube-api-test.py
@@ -11,11 +11,19 @@ def main():
   youtube = build('youtube', 'v3', developerKey=api_key)
 
   # there is no consistent method of getting the id or username of a channel as far as I know
-  request = youtube.channels().list(part="statistics", forUsername="GoogleDevelopers")
-  response = request.execute()
-  print(response)
+  # request = youtube.channels().list(part="statistics", forUsername="INSIDERfood")
+  # response = request.execute()
+  # print(response)
+
+  res = get_videos_from_playlist(youtube, "PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS")
+  print(res)
   
   youtube.close()
+
+def get_videos_from_playlist(youtube, playlist_id):
+  request = youtube.playlistItems().list(part="snippet", playlistId=playlist_id)
+  response = request.execute()
+  return response
   
 if __name__ == "__main__":
   main()
