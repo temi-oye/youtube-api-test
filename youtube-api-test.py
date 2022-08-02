49760b93cd5ed8e8708ec7ee4f9fa9777cb2770b
Temi <temioye30@gmail.com>
2022-08-02 18:06:04 +0100

    get durations from videos in playlist

--- youtube-api-test.py
+++ youtube-api-test.py
@@ -1,3 +1,4 @@
+from stringprep import map_table_b2
 from dotenv import load_dotenv
 import os
 from googleapiclient.discovery import build
@@ -16,28 +17,43 @@ def main():
   # response = request.execute()
   # print(response)
 
-  # res = get_videos_from_playlist(youtube, "PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS")
-  # print(json.dumps(res, indent=4))
 
-  res = get_duration_as_iso(youtube, "DCDe29sIKcE")
+  video_ids = get_videos_ids_from_playlist(youtube, "PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS")
 
-  print(json.dumps(res, indent=4))
+  print(get_duration_as_iso_all(youtube, video_ids))
 
-  
   youtube.close()
 
-def get_videos_from_playlist(youtube, playlist_id):
-  request = youtube.playlistItems().list(part="snippet", playlistId=playlist_id)
-  response = request.execute()
-  return response
+def get_videos_ids_from_playlist(youtube, playlist_id):
+  prev_request = youtube.playlistItems().list(part="snippet", playlistId=playlist_id, maxResults=5)
+  prev_response = prev_request.execute()
+  is_next_page = "nextPageToken" in prev_response
+  videos = prev_response["items"]  
+  # print(json.dumps(videos, indent=4))
+
+  while is_next_page:
+    new_request = youtube.playlistItems().list_next(previous_request=prev_request, previous_response=prev_response)
+    new_response = new_request.execute()
+    is_next_page = "nextPageToken" in new_response
+    videos += new_response["items"]
+    
+    prev_request = new_request
+    prev_response = new_response
+
+  video_ids = map(lambda video: video["snippet"]["resourceId"]["videoId"], videos)
+  # video_ids = map(lambda video:get_duration_as_iso(youtube, video["snippet"]["resourceId"]["videoId"]), videos)
+  # replaces all video ids with their duration (iso) 
+  return list(video_ids)
+
+def get_duration_as_iso_all(youtube, videos_ids):
+  video_iso_all = map(lambda id:get_duration_as_iso(youtube, id), videos_ids)
+  return list(video_iso_all)
 
 def get_duration_as_iso(youtube, video_id):
   request = youtube.videos().list(part="contentDetails", id=video_id)
   response = request.execute()
   iso = response["items"][0]["contentDetails"]["duration"]
   return iso
-
-  
 
   
 if __name__ == "__main__":
